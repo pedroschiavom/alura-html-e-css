c339422dc6c82e1324c8277098b89beb32e72dc1
Pedro Schiavom <pedro_indalecio@hotmail.com>
2020-08-20 21:07:20 -0300

    4 cursos de html e css

---
+++ HTML E CSS PARTE 1/teste/script.js
@@ -0,0 +1,15 @@
+var acc = document.getElementsByClassName("pergunta");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var painel = this.nextElementSibling;
+
+    if (painel.style.maxHeight) {
+      painel.style.maxHeight = null;
+    } else {
+      painel.style.maxHeight = painel.scrollHeight + "px";
+    }
+  });
+}
